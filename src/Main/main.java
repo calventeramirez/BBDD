69c050cc6c5508a9017e7bfc7197593cf6c84871
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2022-03-19 20:26:47 +0100

    Todo  el proyecto subido en la version temprana 0.1

---
+++ src/Main/main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Vista.*;
+
+
+
+/**
+ *
+ * @author Pablo Calvente
+ */
+public class main {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        VistaPrincipal vista = new VistaPrincipal();
+
+        
+        vista.setVisible(true);
+        
+      
+    }
+}
